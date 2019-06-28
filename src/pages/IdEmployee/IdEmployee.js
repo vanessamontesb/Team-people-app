0cd1ae76e70fe7cc8c952cf17e7130fb7e0b2b14
Ana Caicedo <anaccaicedoc@gmail.com>
2019-06-28 10:45:50 -0500

    Employee delete implemented

--- src/pages/IdEmployee/IdEmployee.js
+++ src/pages/IdEmployee/IdEmployee.js
@@ -1,103 +1,102 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 import { API_URL } from "../../constants";
-import EmployeeForm from '../../components/Employees-Container/EmployeeForm/EmployeeForm'
+import EmployeeForm from "../../components/Employees-Container/EmployeeForm/EmployeeForm";
 
+class IdEmployee extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employee: {
+        name: "",
+        job: "",
+        area: "",
+        imgSrc: "",
+        points: "",
+        id: ""
+      }
+    };
+  }
+  handleChange = e => {
+    const newInfo = this.state.employee;
+    newInfo[e.target.name] = e.target.value;
 
-class IdEmployee extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            employee: {
-                name:"",
-                job:"",
-                area:"",
-                imgSrc:"",
-                points:"",
-                id:""
-            }
-        }
-    }
-    handleChange = e => {
-        const newInfo = this.state.employee
-        newInfo[e.target.name] = e.target.value
+    this.setState({
+      employee: newInfo
+    });
+  };
 
-        this.setState({
-            employee : newInfo
-            }
-        )
-    }
+  editEmployee = e => {
+    e.preventDefault();
+    console.log("submit from edit");
+    console.log(this.state.employee);
 
-    editEmployee = e => {
+    const id = this.props.match.params.id;
+
+    axios.put(`${API_URL}/employees/${id}`, {
+      name: this.state.employee.name,
+      job: this.state.employee.job,
+      area: this.state.employee.area,
+      imgSrc: this.state.employee.imgSrc,
+      points: this.state.employee.points
+    });
+  };
+
+    deleteEmployee = e => {
         e.preventDefault()
-        console.log('submit from edit')
-        console.log(this.state.employee)
+        const id = this.props.match.params.id;
 
-        const id = this.props.match.params.id
-
-        axios.put(`${API_URL}/employees/${id}`, {            
-            name:this.state.employee.name,
-            job:this.state.employee.job,
-            area:this.state.employee.area,
-            imgSrc:this.state.employee.imgSrc,
-            points:this.state.employee.points
-        })
+        axios.delete(`${API_URL}/employees/${id}`)
+        .then(res => this.props.history.push(`/employees`))
     }
+  async componentDidMount() {
+    const id = this.props.match.params.id;
 
-    async componentDidMount() {
-        const id = this.props.match.params.id
-        
-        const {data} = await axios.get(`${API_URL}/employees/${id}`)
-        this.setState({employee : data})
-        
-        console.log(this.state.employee)
-    }
+    const { data } = await axios.get(`${API_URL}/employees/${id}`);
+    this.setState({ employee: data });
 
-    
-    render() {
-        const employeeData = this.state.employee
-        return(
-        <div>
-        <img src={employeeData.imgSrc} alt="Profile"/>
+    console.log(this.state.employee);
+  }
+
+  render() {
+    const employeeData = this.state.employee;
+    return (
+      <div>
+        <img src={employeeData.imgSrc} alt="Profile" />
         <p>{employeeData.points}</p>
         <h3>{employeeData.name}</h3>
         <ul>
-            <li>
-                <p>
-                  <span>Job: </span>
-                  <span>{employeeData.job}</span>
-                </p>
-            </li>
-            <li>
-                <p>
-                  <span>Hired in: </span>
-                  <span>{employeeData.area}</span>
-                </p>
-            </li>
+          <li>
+            <p>
+              <span>Job: </span>
+              <span>{employeeData.job}</span>
+            </p>
+          </li>
+          <li>
+            <p>
+              <span>Hired in: </span>
+              <span>{employeeData.area}</span>
+            </p>
+          </li>
         </ul>
-        <EmployeeForm 
-            onChange={this.handleChange} 
-            formValues={this.state.employee}
-            onSubmit={this.editEmployee}
-            />
-            <button  
-            onClick={this.editEmployee} 
-                >Save</button>
-
+        <EmployeeForm
+          onChange={this.handleChange}
+          formValues={this.state.employee}
+          onSubmit={this.editEmployee}
+        />
+        <button onClick={this.editEmployee}>Save</button>
+        <button onClick={this.deleteEmployee}>Delete</button>
 
         <h2>Available Prizes</h2>
         <ul>
-            <li>Prize 1</li>
-            <li>Prize 2</li>
-            <li>Prize 3</li>
-            <li>Prize 4</li>
+          <li>Prize 1</li>
+          <li>Prize 2</li>
+          <li>Prize 3</li>
+          <li>Prize 4</li>
         </ul>
-
-        </div>
-    )
-    
-        
-    }
+      </div>
+    );
+  }
 }
 
-export default IdEmployee
+export default IdEmployee;
