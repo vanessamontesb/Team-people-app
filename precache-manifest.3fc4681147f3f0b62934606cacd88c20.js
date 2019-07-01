083ddbbcff650b91d9177ae121f86e5d3a6174fa
vanessamontesb <vanessamontesb@gmail.com>
2019-06-30 23:14:15 -0500

    Updates

---
+++ precache-manifest.3fc4681147f3f0b62934606cacd88c20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77c2b5431633e5dd0f8272fa180a069",
+    "url": "/Team-people-app/index.html"
+  },
+  {
+    "revision": "1e3cd566777ee0ac3cf1",
+    "url": "/Team-people-app/static/css/main.31da32e7.chunk.css"
+  },
+  {
+    "revision": "c1c93813159cbff1e7c1",
+    "url": "/Team-people-app/static/js/2.e8974815.chunk.js"
+  },
+  {
+    "revision": "1e3cd566777ee0ac3cf1",
+    "url": "/Team-people-app/static/js/main.ab36e237.chunk.js"
+  },
+  {
+    "revision": "e5c96a4375f24df75ee5",
+    "url": "/Team-people-app/static/js/runtime~main.70a77d03.js"
+  }
+]);
